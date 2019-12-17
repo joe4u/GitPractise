bc3d2861c5bd9047a8d025fb6fed2bba41e6702c
joe4u <joe4u@live.cn>
2019-12-17 15:17:05 +0800

    branch1_1

--- GitPractise/Form1.cs
+++ GitPractise/Form1.cs
@@ -16,5 +16,10 @@ namespace GitPractise
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("第一个功能");
+        }
     }
 }
